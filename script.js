90303de546d481a31dfe2a00f8a07e65cb5f3966
Muhammaddavron <muhammaddavron123@gmail.com>
2024-08-09 22:36:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let  num = prompt("Son kiriting")
+if (num % 2 == 0){
+alert("Bu juft son")
+console.log("Bu juft son");
+} else if(num % 2 == 1){ 
+alert("Bu toq son")
+console.log("Bu toq son");
+}
+else{
+    alert('Bu son emas')
+}
